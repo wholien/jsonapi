00fb2dcd4dd65eaf3d3128f34a9d9ea0b7e606fa
Wholien <jchien17@cmc.edu>
2016-06-25 18:02:37 -0700

    refactor dos

--- routes.go
+++ routes.go
@@ -15,23 +15,6 @@ type Route struct {
 
 type Routes []Route
 
-func NewRouter() *mux.Router {
-	router := mux.NewRouter().StrictSlash(true)
-	for _, route := range routes {
-		var handler http.Handler
-		handler = route.HandlerFunc
-		handler = Logger(handler, route.Name)
-
-		router.
-			Methods(route.Method).
-			Path(route.Pattern).
-			Name(route.Name).
-			Handler(handler)
-	}
-
-	return router
-}
-
 var routes = Routes{
 	Route{
 		"Index",
